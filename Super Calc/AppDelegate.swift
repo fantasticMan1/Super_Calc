d16157f6c3d3f5cd3adab056011eba98bc4aebf4
JoshWinebrener <joshua.winebrener@gmail.com>
2017-03-25 08:04:09 -0500

    Initial Commit

---
+++ Super Calc/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Super Calc
+//
+//  Created by Josh Winebrener on 3/25/17.
+//  Copyright © 2017 Josh Winebrener. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Super Calc/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Super Calc
+//
+//  Created by Josh Winebrener on 3/25/17.
+//  Copyright © 2017 Josh Winebrener. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
